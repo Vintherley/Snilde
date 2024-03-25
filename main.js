964c76bed54bb16377476e43d8351a670003e2a9
tanja <tanjamusen1@gmail.com>
2024-03-25 20:45:22 +0100

    flere ændringer

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ document.addEventListener('click', e => {
         dropdown.classList.remove('active')
     })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let slides = document.getElementsByClassName("slide");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 5000); // Skift hvert 5. sekund
+    }
+});
